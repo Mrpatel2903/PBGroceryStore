fbae1fde404a30fd33485fcc16d03742721ae573
Yash Patel <mjpatel210821@gmail.com>
2020-06-26 10:34:07 -0500

    Login Page UI

--- app/src/main/java/com/example/pbgrocerystore/MainActivity.java
+++ app/src/main/java/com/example/pbgrocerystore/MainActivity.java
@@ -2,13 +2,30 @@ package com.example.pbgrocerystore;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.os.Handler;
+import android.view.Window;
+import android.view.WindowManager;
+
+import com.google.firebase.database.GenericTypeIndicator;
 
 public class MainActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        getWindow().requestFeature(Window.FEATURE_NO_TITLE);
+        getWindow().setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN,WindowManager.LayoutParams.FLAG_FULLSCREEN);
         setContentView(R.layout.activity_main);
+
+        new Handler().postDelayed(new Runnable(){
+            @Override
+            public  void run(){
+                startActivity(new Intent(MainActivity.this,LoginActivity.class));
+                finish();
+            }
+            }
+        , 2000);
     }
 }
