3c593a81ea092097f077d3723a7dc8bb6100f89a
Yash Patel <mjpatel210821@gmail.com>
2020-06-26 10:36:22 -0500

    Login Page UI

---
+++ app/src/main/java/com/example/pbgrocerystore/LoginActivity.java
@@ -0,0 +1,42 @@
+package com.example.pbgrocerystore;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
+
+public class LoginActivity extends AppCompatActivity {
+
+    private EditText emailID, passwordID;
+    private TextView forgetID, NewAccountID;
+    private Button LoginBtn;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_login1);
+
+        emailID= findViewById(R.id.emailID);
+        passwordID = findViewById(R.id.passwordID);
+        forgetID = findViewById(R.id.forgetID);
+        NewAccountID = findViewById(R.id.NewAccountID);
+        LoginBtn = findViewById(R.id.LoginBtn);
+
+        NewAccountID.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                startActivity(new Intent(LoginActivity.this, RegisterUserAcitivity.class));
+            }
+        });
+
+       forgetID.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                startActivity(new Intent(LoginActivity.this, ForgetPasswordActivity.class));
+            }
+        });
+    }
+}
